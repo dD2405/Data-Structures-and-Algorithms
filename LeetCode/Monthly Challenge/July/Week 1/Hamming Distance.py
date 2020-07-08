cd119451f74470c53734062f4fe480ef71702969
Deepak Das <deepakdas1998@gmail.com>
2020-07-08 17:53:28 +0530

    Update Hamming Distance.py

--- LeetCode/Monthly Challenge/July/Week 1/Hamming Distance.py
+++ LeetCode/Monthly Challenge/July/Week 1/Hamming Distance.py
@@ -1,4 +1,4 @@
-'''he Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+'''The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
 
 Given two integers x and y, calculate the Hamming distance.
 
