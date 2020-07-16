872b1c4b8110bd5b007a54af3ab9e06c78d7af8c
Deepak Das <deepakdas1998@gmail.com>
2020-07-16 13:41:07 +0530

    Update Climbing Stairs.py

--- LeetCode/Dynamic Programming/Climbing Stairs.py
+++ LeetCode/Dynamic Programming/Climbing Stairs.py
@@ -24,6 +24,7 @@ Constraints:
 
     1 <= n <= 45 '''
 
+# Fibonacci Sequence
 class Solution:
     def climbStairs(self, n: int) -> int:
         
@@ -35,7 +36,29 @@ class Solution:
         
         return res
         
+# Recursion With Memoization
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = [0] * (n+1)
+        return self.helper(0,n,memo)
         
+    def helper(self, i, n, memo):
+        if i > n:
+            return 0
+
+        if i == n:
+            return 1
+
+        if memo[i] > 0:
+            return memo[i]
+
+        memo[i] = self.helper(i+1, n, memo) + self.helper(i+2, n, memo)
+        return memo[i]
+        
+# Dynamic Programming 
+
+        
+
         
         
 
